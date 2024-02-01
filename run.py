258743e5b7eb659d9166c85a0ac0b36da2649a83
EddieCodeHub <eddiemeekin@gmail.com>
2024-02-01 19:19:31 +0000

    function: create function to print intro message and instructions

--- run.py
+++ run.py
@@ -8,3 +8,15 @@ ship_symbol = "S"
 hit_symbol = "X"
 miss_symbol = "O"
 empty_symbol = "-"
+
+def game_intro_message():
+    """
+    Prints Welcome message and instructions to play the game
+    """
+    print("Welcome to battleships!")
+    print("Column co-ordinates go from left to right")
+    print("Row co-ordinates go from top to bottom")
+    print("The Board is a 5x5 grid, Starting at the top left at Co-ordinate: row: 1, column: A")
+    print("Each player had 3 ships each")
+    print("First to sink all enemy ships wins!")
+    print("To replay the game, select run program")
