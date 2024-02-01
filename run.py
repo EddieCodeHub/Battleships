4e9ec0f05684059e74aa6a15e106f8eb844574f9
EddieCodeHub <eddiemeekin@gmail.com>
2024-02-01 19:03:25 +0000

    import random and create variables to generate grid

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Import random to create CPU player moves
+import random
+
+# Create Variables to generate game 
+grid_size = 5
+ship_num = 3
+ship_symbol = "S"
+hit_symbol = "X"
+miss_symbol = "O"
+empty_symbol = "-"
