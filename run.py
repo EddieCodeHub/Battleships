6e90546ec804d2fc6a8fe14f8890265a5bf706e0
EddieCodeHub <eddiemeekin@gmail.com>
2024-02-07 16:17:11 +0000

    fix errors from python linter

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # Import random to create CPU player moves
 from random import randint
 
-# Create Variables to generate game 
+# Create Variables to generate game
 player_ship_num = 3
 cpu_ship_num = 3
 ship_symbol = "S"
@@ -21,10 +21,10 @@ def game_intro_message():
     print("Welcome to battleships!")
     print("Column co-ordinates go from left to right")
     print("Row co-ordinates go from top to bottom")
-    print("The Board is a 5x5 grid, Starting at the top left at Co-ordinate: row: 1, column: A")
-    print("Each player had 3 ships each")
+    print("The Board is a 5x5 grid")
+    print("Starting at the top left at Co-ordinate: row: 1, column: A")
+    print("Each player has 3 ships each")
     print("First to sink all enemy ships wins!")
-
 
 
 def print_player_grid():
@@ -33,10 +33,10 @@ def print_player_grid():
     """
     player_grid = [["-" for _ in range(5)] for _ in range(5)]
 
-    print(" ", end=" ") 
+    print(" ", end=" ")
     for col in col_labels:
         print(col, end=" ")
-    print() 
+    print()
     for i, row in enumerate(player_grid):  # enumerate adds numbers to the rows
         print(row_labels[i], end=" ")
         for cell in row:
@@ -44,6 +44,7 @@ def print_player_grid():
         print()
     print("Player 1, place your ships!")
     return player_grid
+
 
 def get_ship_coordinates():
     """
@@ -67,9 +68,15 @@ def get_ship_coordinates():
     return player_ship_coordinates
 
 
-def validate_ship_coordinates(player_ship_coordinates, player_grid, col_labels, row_labels):
+def validate_ship_coordinates(
+    player_ship_coordinates,
+    player_grid,
+    col_labels,
+    row_labels
+):
     """
-    Takes in ship co-ordinates and validates them and prints the grid with ships placed
+    Takes in ship co-ordinates and validates them
+    and prints the grid with ships placed
     """
     for row, col in player_ship_coordinates:
         row_index = row - 1
@@ -79,10 +86,10 @@ def validate_ship_coordinates(player_ship_coordinates, player_grid, col_labels, 
             get_ship_coordinates()
         player_grid[row_index][col_index] = ship_symbol
     print("Ships placed!")
-    print(" ", end=" ") 
+    print(" ", end=" ")
     for col in col_labels:
         print(col, end=" ")
-    print() 
+    print()
     for i, row in enumerate(player_grid):
         print(row_labels[i], end=" ")
         for cell in row:
@@ -91,10 +98,10 @@ def validate_ship_coordinates(player_ship_coordinates, player_grid, col_labels, 
     return player_grid
 
 
-
 def generate_cpu_ships(col_labels, row_labels):
     """
-    Generates CPU ships and places them on the grid but not visible to the player
+    Generates CPU ships and places them on the grid
+    but not visible to the player
     """
     cpu_grid = [["-" for _ in range(5)] for _ in range(5)]
     # Create a new grid to store the shots
@@ -106,23 +113,24 @@ def generate_cpu_ships(col_labels, row_labels):
             if cpu_grid[row][col] != ship_symbol:
                 cpu_grid[row][col] = ship_symbol
                 break
-    
+
     # Print the shot grid instead of the cpu grid
     print("  ", end="")
     for col in col_labels:
         print(col, end=" ")
-    print() 
+    print()
     for i, row in enumerate(shot_grid):
         print(row_labels[i], end=" ")
         for cell in row:
             print(cell, end=" ")
         print()
-    return cpu_grid, shot_grid 
+    return cpu_grid, shot_grid
 
 
 def get_player_guess():
     """
-    Takes in player input to generate guesses and print appropriate symbols on the grid
+    Takes in player input to generate guesses
+    and print appropriate symbols on the grid
     """
     while True:
         print("Enter your guess!")
@@ -138,9 +146,17 @@ def get_player_guess():
             player_guess = (row, col)
             return player_guess
 
-def validate_player_guess(player_guess, cpu_grid, shot_grid, col_labels, row_labels):
+
+def validate_player_guess(
+    player_guess,
+    cpu_grid,
+    shot_grid,
+    col_labels,
+    row_labels
+):
     """
-    Takes in player guess and validates them and prints the grid with hits and misses
+    Takes in player guess and validates them
+    and prints the grid with hits and misses
     """
     global cpu_ship_num
     row, col = player_guess
@@ -156,17 +172,17 @@ def validate_player_guess(player_guess, cpu_grid, shot_grid, col_labels, row_lab
     else:
         print("Miss!")
         shot_grid[row_index][col_index] = miss_symbol
-    print(" ", end=" ") 
+    print(" ", end=" ")
     for col in col_labels:
         print(col, end=" ")
-    print() 
+    print()
     for i, row in enumerate(shot_grid):
         print(row_labels[i], end=" ")
         for cell in row:
             print(cell, end=" ")
         print()
     return shot_grid
-    
+
 
 def get_cpu_guess():
     """
@@ -176,19 +192,26 @@ def get_cpu_guess():
         row = randint(1, 5)
         col = randint(1, 5)
         cpu_guess = (row, col)
-        # If the guess is not in the set of previous guesses, add it to the set and return it
+        # If the guess is not in the set of previous guesses,
+        # add it to the set and return it
         if cpu_guess not in cpu_guesses:
             cpu_guesses.add(cpu_guess)
             return cpu_guess
 
 
-def validate_cpu_guess(cpu_guess, player_grid, col_labels, row_labels):
+def validate_cpu_guess(
+    cpu_guess,
+    player_grid,
+    col_labels,
+    row_labels
+):
     """
-    Takes in cpu guess and validates them and prints the grid with hits and misses
+    Takes in cpu guess and validates them
+    and prints the grid with hits and misses
     """
     global player_ship_num
     row, col = cpu_guess
-    row_index = row - 1 
+    row_index = row - 1
     col_index = col - 1
     if player_grid[row_index][col_index] == miss_symbol:
         get_cpu_guess()
@@ -202,13 +225,14 @@ def validate_cpu_guess(cpu_guess, player_grid, col_labels, row_labels):
     print("  ", end="")
     for col in col_labels:
         print(col, end=" ")
-    print() 
+    print()
     for i, row in enumerate(player_grid):
         print(row_labels[i], end=" ")
         for cell in row:
             print(cell, end=" ")
         print()
     return player_grid
+
 
 def main_loop():
     """
@@ -217,15 +241,32 @@ def main_loop():
     game_intro_message()
     player_grid = print_player_grid()
     player_ship_coordinates = get_ship_coordinates()
-    validate_ship_coordinates(player_ship_coordinates, player_grid, col_labels, row_labels)
+    validate_ship_coordinates(
+        player_ship_coordinates,
+        player_grid,
+        col_labels,
+        row_labels
+    )
     cpu_grid, shot_grid = generate_cpu_ships(col_labels, row_labels)
     player_guess = get_player_guess()
-    validate_player_guess(player_guess, cpu_grid, shot_grid, col_labels, row_labels)
+    validate_player_guess(
+        player_guess,
+        cpu_grid,
+        shot_grid,
+        col_labels,
+        row_labels
+    )
     cpu_guess = get_cpu_guess()
     validate_cpu_guess(cpu_guess, player_grid, col_labels, row_labels)
     while player_ship_num > 0 and cpu_ship_num > 0:
         player_guess = get_player_guess()
-        validate_player_guess(player_guess, cpu_grid, shot_grid, col_labels, row_labels)
+        validate_player_guess(
+            player_guess,
+            cpu_grid,
+            shot_grid,
+            col_labels,
+            row_labels
+        )
         cpu_guess = get_cpu_guess()
         validate_cpu_guess(cpu_guess, player_grid, col_labels, row_labels)
         print(f"Player has {player_ship_num} ships left")
@@ -238,5 +279,3 @@ def main_loop():
 
 
 main_loop()
-
-
